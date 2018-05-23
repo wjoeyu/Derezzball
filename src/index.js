76881d5556ca08710c5d59acdf81bd17ab55ce2d
wjoeyu <wjoeyu@gmail.com>
2018-05-23 16:33:31 -0400

    added test of files for three.js

--- src/index.js
+++ src/index.js
@@ -1,11 +1,27 @@
-import _ from 'lodash';
+import * as THREE from 'three';
 
-function component () {
-  const element = document.createElement('div');
+const scene = new THREE.Scene();
+const camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 0.1, 1000 );
 
-  element.innerHTML = _.join(['Hello', 'webpack'], ` `);
+const renderer = new THREE.WebGLRenderer();
+renderer.setSize( window.innerWidth, window.innerHeight );
 
-  return element;
+const geometry = new THREE.BoxGeometry( 1, 1, 1 );
+const material = new THREE.MeshBasicMaterial( { color: 0x00ff00 } );
+const cube = new THREE.Mesh( geometry, material );
+scene.add( cube );
+
+camera.position.z = 5;
+
+function animate() {
+	requestAnimationFrame( animate );
+
+  // cube.rotation.x += 0.1;
+  // cube.rotation.y += 0.1;
+	renderer.render( scene, camera );
 }
 
-document.body.appendChild(component());
+animate();
+
+
+document.body.appendChild(renderer.domElement);
